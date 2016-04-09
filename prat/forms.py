2fe97caa6c881cd72d8823e9e869e4ea4a69b8bf
theadriann <adriann54@gmail.com>
2016-04-09 19:49:24 +0300

    Add Edit Profile w/o already filled forms

---
+++ prat/forms.py
@@ -0,0 +1,22 @@
+from django.forms import Form, CharField, Textarea, PasswordInput, ImageField,\
+            EmailField, ValidationError, ChoiceField, DateField, SelectDateWidget
+from django.contrib.auth.models import User
+
+class EditProfileForm(Form):
+    first_name = CharField (max_length = 50, required = False)
+    last_name  = CharField (max_length = 50, required = False)
+    email      = EmailField (required = False)
+    birthday   = DateField (
+            required = False,
+            widget = SelectDateWidget(
+                empty_label = ("Choose Year", "Choose Month", "Choose Day"),
+            )
+    )
+    gender    = ChoiceField (choices = (('M', 'Male'), ('F', 'Female')), required = False)
+    avatar    = ImageField (required = False)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if email and User.objects.filter(email=email).count():
+            raise ValidationError('Email already in use!')
+        return email
--- prat/views.py
+++ prat/views.py
@@ -3,9 +3,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import get_object_or_404, redirect, render
 
-# Prat
+# Prat Models
 from django.contrib.auth.models import User
 from prat.models import UserProfile, Task
+
+# Prat Forms
+from prat.forms import EditProfileForm
 
 def index(request):
     context = {
@@ -25,7 +28,7 @@ def viewProfile(request, username = None):
         user = User.objects.filter(username = username).first()
     else:
         user = request.user
-    
+
     context = {
         'user': user,
         'profile': user.profile
@@ -34,3 +37,37 @@ def viewProfile(request, username = None):
     if request.user.username == username or not username:
         return render(request, 'myprofile.html', context)
     return render(request, 'profile.html', context)
+
+@login_required
+def editProfile(request):
+    if request.method == 'GET':
+        form = EditProfileForm()
+        context = {
+            'form': form,
+        }
+
+        return render(request, 'edit_profile.html', context)
+    elif request.method ==  'POST':
+        form = EditProfileForm(request.POST, request.FILES)
+        user = request.user
+        profile = user.profile
+
+        if form.is_valid():
+            if form.cleaned_data['first_name']:
+                profile.first_name = form.cleaned_data['first_name']
+            if form.cleaned_data['last_name']:
+                profile.last_name = form.cleaned_data['last_name']
+            if form.cleaned_data['birthday']:
+                profile.birthday = form.cleaned_data['birthday']
+            if form.cleaned_data['gender']:
+                profile.gender = form.cleaned_data['gender']
+            if form['avatar'].value():
+                profile.avatar = form['avatar'].value()
+            if form.cleaned_data['email']:
+                user.email = form.cleaned_data['email']
+                user.save()
+            profile.save()
+        else:
+            context = {'form': form}
+            return render(request, 'edit_profile.html', context)
+        return redirect('viewProfile')
